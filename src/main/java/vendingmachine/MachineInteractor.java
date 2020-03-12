f4d3ec311b317ee86d8d1006db91d4a8af28d10a
haidichen <chenhaidi43@163.com>
2020-03-11 21:24:21 -0500

    refactored the MachineInteractor class

--- src/main/java/vendingmachine/MachineInteractor.java
+++ src/main/java/vendingmachine/MachineInteractor.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class MachineInteractor {
 
+  private static final String REFUND_OPTION = "r";
   private VendingMachine machine;
   private OutputPlatform output;
   private InputPlatform input;
-
   private boolean refundRequested = false;
 
   public MachineInteractor(VendingMachine machine) {
@@ -22,40 +22,40 @@ public class MachineInteractor {
     this.output = output;
   }
 
-  private int insertCoins(int moneyInMachine) {
-    output.printInstruction();
-    String coinInserted = input.getStringInput();
-    if (coinInserted.equals("r")) {
-      output.printRefundInfo(machine.refund());
-      refundRequested = true;
-    }
-    else {
-      machine.takeCoin(Integer.parseInt(coinInserted));
-    }
+  private int moneyInMachineSoFar() {
     return machine.getRemainingChange();
   }
 
-  private void buyItem(String itemName, int totalMoneyInMachine) {
-    try {
-      int change = machine.popItem(itemName);
-      output.printNameOfItemPurchasedAndChange(itemName, change);
+  private String askUserToPayOrQuitWithRefund() {
+    output.printInstruction();
+    return input.getStringInput();
+  }
+
+  private void takeTheCoinOrQuitWithRefund(String payOrRefund) {
+    if (selectedRefundOption(payOrRefund)) {
+      processRefund();
     }
-    catch (VendingMachine.NotEnoughMoney e) {
-      output.printExceptionMessage(e);
-      output.printMoneyPaid(machine.getRemainingChange());
-      int currentMoneyInMachine = insertCoins(totalMoneyInMachine);
-      if (!refundRequested) {
-        buyItem(itemName, currentMoneyInMachine);
-      }
+    else {
+      machineTakesTheCoin(payOrRefund);
     }
-    catch (VendingMachine.StockEmpty ex) {
-      output.printExceptionMessage(ex);
-    }
+  }
+
+  private void processRefund() {
+    output.printRefundInfo(machine.refund());
+    refundRequested = true;
+  }
+
+  private boolean selectedRefundOption(String payOrRefund) {
+    return payOrRefund.equals(REFUND_OPTION);
+  }
+
+  private void machineTakesTheCoin(String payOrRefund) {
+    machine.takeCoin(Integer.parseInt(payOrRefund));
   }
 
   public void start() {
     displayItemsInMachine();
-    chooseOneItemToBuy();
+    letUserChooseOneItemToBuy();
   }
 
   private void displayItemsInMachine() {
@@ -63,10 +63,64 @@ public class MachineInteractor {
     output.printItemsNamePriceAndStock(machine.getProducts());
   }
 
-  private void chooseOneItemToBuy() {
+  private void letUserChooseOneItemToBuy() {
     output.printAskingForSelection();
-    String itemName = input.getStringInput();
-    buyItem(itemName, 0);
+    askUserToSelectOneItemToBuy();
+  }
+
+  private void askUserToSelectOneItemToBuy() {
+    String selectedItemName = input.getStringInput();
+    buyItem(selectedItemName, 0);
+  }
+
+  private void buyItem(String itemName, int moneyPaidSoFar) {
+    try {
+      returnTheSelectedItemAndChangeIfAny(itemName);
+    }
+    catch (VendingMachine.NotEnoughMoney e) {
+      informUserOfNextSteps(e, itemName, moneyPaidSoFar);
+    }
+    catch (VendingMachine.StockEmpty ex) {
+      informUserOfItemSoldOutAndQuit(ex);
+    }
+  }
+
+  private void informUserOfNextSteps(
+      Exception e, String itemName, int moneyPaidSoFar) {
+    tellUserThatMoneyWasNotEnoughToBuyTheItem(e);
+    int currentMoneyPaidSoFar = afterInsertCoinsOrAskRefund(moneyPaidSoFar);
+    continuePurchaseOrQuit(itemName, currentMoneyPaidSoFar);
+  }
+
+  private void informUserOfItemSoldOutAndQuit(Exception e) {
+    output.printExceptionMessage(e);
+  }
+
+  private int afterInsertCoinsOrAskRefund(int moneyInMachine) {
+    String payOrRefund = askUserToPayOrQuitWithRefund();
+    takeTheCoinOrQuitWithRefund(payOrRefund);
+    return moneyInMachineSoFar();
+  }
+
+  private void continuePurchaseOrQuit(String itemName, int moneyPaidSoFar) {
+    if (refundRequested) { 
+      refundRequested = false;  // next purchasing has no requested refund
+      return; // quit the current purchasing
+    } 
+    else {
+      buyItem(itemName, moneyPaidSoFar);
+    }
+  }
+
+  private void tellUserThatMoneyWasNotEnoughToBuyTheItem(Exception e) {
+    output.printExceptionMessage(e);
+    output.printMoneyPaid(machine.getRemainingChange());
+  }
+
+  private void returnTheSelectedItemAndChangeIfAny(String itemName) 
+      throws VendingMachine.NotEnoughMoney, VendingMachine.StockEmpty {
+    int change = machine.popItem(itemName);
+    output.printNameOfItemPurchasedAndChange(itemName, change);
   }
 
 }
